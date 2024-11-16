78cc2684ceb8df0e663dc473763ee69f47b76c79
Keith Tweed <ktweed@ualberta.ca>
2024-11-15 18:48:58 -0700

    GUI updates

--- gui/main.py
+++ gui/main.py
@@ -2,33 +2,56 @@ import streamlit as st
 
 
 def setup():
+    """Define the initial setup page to collect required parameters"""
     st.header("NAThacks24 Artists Interface")
     mode = st.selectbox(
         "Select Mode",
-        ["Data Collection", "Data Analysis", "Data Visualization"],
+        ["Collection", "Analysis", "Visualization"],
     )
     filename = st.text_input("Enter File Name")
 
+    # Save the selected mode and filename to session state and rerun to load next page
     if st.button("Enter"):
-        st.session_state.mode = mode
-        st.session_state.filename = filename
-        st.rerun()
+        if len(filename) > 0:
+            st.session_state.mode = mode
+            st.session_state.filename = filename
+            st.rerun()
 
 
 col_main = st.Page("collection/col_main.py", title="Data Collection")
 
 collection_pages = [col_main]
+visualization_pages = []
+analysis_pages = []
 
 
-if __name__ == "__main__":
-    logo_path = "gui/images/logo.png"
-    st.logo(logo_path)
-    if "mode" in st.session_state:
-        if st.session_state.mode == "Data Collection":
-            pg = st.navigation()
-        elif st.session_state.mode == "Data Analysis":
-            st.write("Data Analysis Mode")
-        elif st.session_state.mode == "Data Visualization":
-            st.write("Data Visualization Mode")
-    else:
-        setup()
+# Define modes
+MODES = [None, "Collection", "Analysis", "Visualization"]
+
+# Initialise available pages for mode
+page_dict = {}
+
+# Set the logo
+logo_path = "gui/images/logo.png"
+st.logo(logo_path)
+
+# Initialise params
+if "mode" not in st.session_state:
+    st.session_state.mode = None
+if "filename" not in st.session_state:
+    st.session_state.filename = None
+
+# Define available pages based on mode
+if st.session_state.mode in ["Collection", "Debug"]:
+    page_dict["Collection"] = collection_pages
+elif st.session_state.mode in ["Analysis", "Debug"]:
+    page_dict["Analysis"] = analysis_pages
+elif st.session_state.mode in ["Visualization", "Debug"]:
+    page_dict["Visualization"] = visualization_pages
+
+# Populate nav or boot to setup
+if len(page_dict) > 0:
+    pg = st.navigation(page_dict)
+    pg.run()
+else:
+    setup()
